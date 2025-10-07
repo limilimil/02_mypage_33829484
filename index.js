31832cbafafd4201613789972d634a2dfd971f74
lcann <liamcannon93@gmail.com>
2025-10-07 15:51:28 +0100

    Initial version

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require("http"); 
+const port = 8000; 
+
+http.createServer(function(req, res) { 
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<!DOCTYPE html><html><head><title>My web page</title></head><body><h1>My web page</h1> <h2>Something</h2><p>Wow, this is great!</p></body</html>");
+    }).listen(port, function() { 
+        console.log(`Node server is running on port ${port}...`); 
+}); 
