485218403c7c0e9d72aa082a9c4a350d704d3293
lcann <liamcannon93@gmail.com>
2025-10-18 16:34:34 +0100

    Moved html to a separate file, amended html content and readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MyPage
-A very small node.js web application.
+A very small node.js web application, developed for the Dynamic Web Applications module.
--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 var http = require("http"); 
+var fs = require("fs");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`
-        <!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <title>My Web Page</title>
-          </head>
-          <body>
-            <h1>My web page</h1> 
-            <h2>About</h2>
-            <p>My name is Liam, and I graduated from one of Canada's top business schools with really good grades.</p>
-          </body>
-        </html>`);
-    }).listen(port, function() { 
-        console.log(`Node server is running on port ${port}...`); 
+  fs.readFile("mypage.html", (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("505 Internal Server Error");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
+
+}).listen(port, function() { 
+  console.log(`Node server is running on port ${port}...`); 
 }); 
